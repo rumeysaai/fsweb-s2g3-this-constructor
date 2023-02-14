c2096127de1b4fdb805ebbb2415a53bd80ee5bae
rumeysaai <rumeysaakgunn@gmail.com>
2023-02-14 17:21:38 +0300

    S2G3 Gorev

--- index.js
+++ index.js
@@ -14,10 +14,23 @@
         + Bu `isim` ve `yas` i içeren bir string döndürmelidir Örnek: "Mary, 50"
 */
 
-function Kisi(/* kodlar buraya */) {
-  /* kodlar buraya */
-}
+function Kisi(isim, yas) {
+  this.isim = isim;
+  this.yas = yas;
+  this.mide = [];
+  this.ye = function(yemek){
+    if(this.mide.length<11)
+      this.mide.push(yemek);
+  }
+  this.bosalt = function(){
+     this.mide = [];
+  }
+  this.toString = function(){
 
+    return `${this.isim}, ${this.yas}`;
+  }
+  
+}
 
 /*
   GÖREV 2
@@ -35,8 +48,26 @@ function Kisi(/* kodlar buraya */) {
         +  "x milde benzinim bitti!" x değişkeni `odometer` daki sayı olmalıdır.
 */
 
-function Araba(/* kodlar buraya */) {
-  /* kodlar buraya */
+function Araba(model, milesPerGallon) {
+  this.model= model;
+  this.milesPerGallon=milesPerGallon;
+  this.tank = 0;
+  this.odometer= 0 ;
+  this.fill = function(gallons){
+    return this.tank += gallons;
+  }
+  this.drive = function(distance){
+    
+    this.odometer+=distance;
+    this.tank-= Math.round(distance / this.milesPerGallon);
+    if(this.tank<=0)
+    {
+      const  extraMile = this.tank * this.milesPerGallon;
+      this.odometer+=extraMile;
+      this.tank=0;
+      console.log (`${this.odometer} milde benzinim bitti!`);
+    }
+  }
 }
 
 
